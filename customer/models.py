067ee78e8c1c1921cbd8219046bb701c98ee2739
rafaelguibson <rafaelguibsons@gmail.com>
2025-11-09 21:10:01 -0300

    [🔧 Fix]    ➡ Correction class Meta verbose atribute.

--- customer/models.py
+++ customer/models.py
@@ -39,7 +39,7 @@ class Customer(models.Model):
 
     class Meta:
         verbose_name = 'Cliente',
-        verbose_name_pural = 'Clientes'
+        verbose_plural_name = 'Clientes'
 
     def __str__(self):
         return self. name
