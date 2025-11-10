0365dbbe9693a0b20b4b42c0bd0e9f2df818b0ad
rafaelguibson <rafaelguibsons@gmail.com>
2025-11-10 08:40:14 -0300

    [➕ Adjusted]    ➡ Configured django admin file for vehicle and vehicleType.

--- vehicle/admin.py
+++ vehicle/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from vehicle.models import Vehicle, VehicleType
 
-# Register your models here.
+@admin.register(VehicleType)
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['license_plate', 'brand', 'model', 'color']
+    search_fields = ['license_plate', 'model']
+    list_filter = ['vehicle_type']
--- vehicle/apps.py
+++ vehicle/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class VehicleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vehicle'
+    verbose_name = 'Veículo'
+    verbose_name_plural = 'Veículos'
